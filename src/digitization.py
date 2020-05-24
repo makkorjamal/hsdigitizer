84cc9ab155db61e035af12228f6d7864cdb6d443
JaMaL <makkor@uni-bremen.de>
2020-05-24 10:49:02 +0200

    2nd commit

---
+++ src/digitization.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+from __future__ import print_function, division
+import concurrent.futures
+import matplotlib
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+# from matplotlib.widgets import Slider, Button, RadioButtons, TextBox
+from spectrum import Spectrum
+from jsonparser import JsonParser
+
+class Digitizer():
+
+    def __init__(self, dpath, savepath):
+
+        self.dpath = dpath
+        self.sp_range = [1218, 1155]
+        self.savepath = savepath
+        self.parallelize()
+
+    def parallelize(self):
+        self.img_names = os.listdir(self.dpath)
+
+        self.spectrums = []
+        with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+            for spectrum in executor.map(self.digitize_spectrum, self.img_names):
+                self.spectrums.append(spectrum)
+        json_parser = JsonParser('data',self.spectrums)
+        json_parser.save_json()
+
+    def digitize_spectrum(self,img_fname):
+
+        # self.img = cv2.imread(os.path.join(self.dpath, img_fname))
+        img = cv2.imread(os.path.join(self.dpath, img_fname))
+        self.img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.b, self.g, _ = cv2.split(self.img)
+        self.digitized_spectrum = []
+        for i in range(self.img.shape[1]):
+            self.digitized_spectrum.append(self.digitize_point(i))
+        if self.digitized_spectrum:
+            impath = img_fname
+            sp_name = img_fname[:-4]+'_digitized'+'.dat'
+            spath = os.path.join(self.savepath,sp_name)
+            with open(spath,'w') as f:
+                for i in self.digitized_spectrum:
+                    f.write('%4.2f\n'%i)
+            print('{}'.format(spath))
+            return  Spectrum(impath,sp_name, sp_range = self.sp_range)
+
+    def digitize_point(self, s):
+        x = np.arange(self.img.shape[0])
+        y = np.divide(self.b[:, s],self.g[:, s],where = self.g[:,s] != 0)
+        cond = y>(np.max(y)*0.80)
+        # cond =(np.max(y)*0.90)> y>(np.max(y)*0.80)
+        return np.mean(x[cond])
+
+if __name__ == '__main__':
+    # dpath = 'images/'
+    savepath = 'data/'
+    dpath = '/mnt/740617C970FA5889/scroll1_21_aout/'
+    # dpath = '/home/jamal/spectra'
+    Digitizer(dpath, savepath)
+
+
+
+
---
+++ src/jsonparser.py
@@ -0,0 +1,32 @@
+import json
+import os
+from spectrum import Spectrum
+class JsonParser:
+    """Class to save spectra in a json file"""
+
+    def __init__(self,jpath, spectrums = []):
+        self.spectrums = spectrums
+        self.jpath = jpath
+
+    def save_json(self):
+        #save the spectrum.py object in a json file
+
+       with open(os.path.join(self.jpath,"spectra_file.json"), "w") as write_file:
+           json.dump([obj.__dict__ for obj in self.spectrums], write_file)
+
+    def read_json(self,filename):
+        #read the saved json.
+        #saved in a Spectrum object.
+
+        with open(os.path.join(self.jpath,filename), "r") as read_file:
+            data = json.load(read_file)
+        for d in data:
+            sp = Spectrum(d['img_name'],d['sp_name'],d['calsp_name'], d['calsplines_name'], d['sp_range'])
+            self.spectrums.append(sp)
+        return self.spectrums 
+if __name__ == '__main__':
+    #spectrums = []
+    jsparser = JsonParser('data/',[])
+    data = jsparser.read_json('spectra_file.json')
+    for spectrum in data:
+        print(spectrum.__dict__)
